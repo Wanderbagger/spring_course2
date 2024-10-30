ea514def19bed65293d3342de5dd7db25973ce47
Wanderbagger <ronikut1988@mail.ru>
2024-10-30 13:47:24 +0300

    Учеба Спринг продолжение урок 1.16

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,15 +1,25 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
+    @Autowired
     private Pet pet;
     private String surname;
     private int age;
 
+//    @Autowired
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
 
     public Person() {
         System.out.println("Person bean is created");
     }
-
+//    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set pet");
         this.pet = pet;
