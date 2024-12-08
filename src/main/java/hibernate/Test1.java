4c7ab8efe4453f7d5acdd49b08de1ca77fb97d02
Wanderbagger <ronikut1988@mail.ru>
2024-12-08 20:25:02 +0300

    продолжение обучения
    Продолжение обучения hibernate

--- src/main/java/hibernate/Test1.java
+++ src/main/java/hibernate/Test1.java
@@ -9,8 +9,6 @@ import org.hibernate.cfg.Configuration;
 public class Test1 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
-
-
                     .configure("hibernate.cfg.xml")
                     .addAnnotatedClass(Employee.class)
                     .buildSessionFactory();
@@ -20,9 +18,7 @@ public class Test1 {
                     "IT", 500);
             session.beginTransaction();
             session.save(employee);
-            session.getTransaction();
-        } catch( Exception e) {
-            throw new RuntimeException(e);
+            session.getTransaction().commit();
         } finally {
             factory.close();
         }
